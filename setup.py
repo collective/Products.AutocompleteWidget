1e007b983009bbfb8d8e19c233056c86b9ddf382
Harald Friessnegger <harald@webmeisterei.com>
2010-01-25 20:50:44 +0000

    basic egg structure
    svn path=/MoreFieldsAndWidgets/Products.AutocompleteWidget/trunk/; revision=12141

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+version = '1.2.2-dev'
+
+setup(name='Products.AutocompleteWidget',
+      version=version,
+      description="Archetypes autocomplete widget with support for String-, Lines- and ReferenceFields",
+      #long_description=open("README.txt").read() + "\n" +
+      #                 open("CHANGES.txt").read(),
+      long_description="XXX",
+      classifiers=[
+        "Framework :: Zope2",
+        "License :: OSI Approved :: Zope Public License",
+        "Programming Language :: Python",
+      ],
+      keywords='Zope Plone Archetypes',
+      author="Jonathan Riboux, Alec Mitchell and contributors",
+      author_email="jonathan.riboux@quadra-informatique.fr, plone-developers@lists.sourceforge.net",
+      url='http://plone.org/products/autocompletewidget/',
+      license='ZPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        # 'Zope2',
+      ],
+)
