6302883f8f9b9e08d3a562dc608de5e9e77ebf8e
Harald Friessnegger <harald@webmeisterei.com>
2010-01-26 16:54:34 +0000

    ade widget work with vocabularies containing non-ascii characters too
    svn path=/MoreFieldsAndWidgets/Products.AutocompleteWidget/branches/improved_multivalued_fields_management/; revision=12161

--- Products/AutocompleteWidget/AutocompleteWidget.py
+++ Products/AutocompleteWidget/AutocompleteWidget.py
@@ -1,11 +1,12 @@
 from AccessControl import ClassSecurityInfo
 from Products.Archetypes.public import StringWidget
+from Products.CMFPlone.utils import safe_unicode
 from logging import getLogger
 logger = getLogger('AutocompleteWidget')
 
 class AutocompleteWidget(StringWidget):
     security = ClassSecurityInfo()
-    
+
     _properties = StringWidget._properties.copy()
     _properties.update({
         'macro' : "autocomplete",
@@ -25,56 +26,57 @@ class AutocompleteWidget(StringWidget):
     security.declarePrivate('keyword_from_value')
     def keyword_from_value(self, value, instance, field):
         logger.debug("AutocompleteWidget.keyword_from_value is deprecated")
-        vocab = field.Vocabulary(instance) # potentially expensive 
-        if self.actb_filter_bogus: 
+        vocab = field.Vocabulary(instance) # potentially expensive
+        if self.actb_filter_bogus:
             # delete bogus keywords
             return vocab.getKey(value.strip(), None)
-        else: 
+        else:
             # keep bogus keywords ; if no keyword, use value
             return vocab.getKey(value.strip(), value.strip())
-    
+
     security.declarePublic('process_form')
-    def process_form(self, instance, field, form, empty_marker=None, 
+    def process_form(self, instance, field, form, empty_marker=None,
                        emptyReturnsMarker=False):
         """ process the string into a list """
 
         value = form.get(field.getName(), None)
+        value = value and safe_unicode(value) or None
 
         # no value!
         if not value:
             return (field.type=='lines' or field.multiValued) and [] or '', {}
-            
+
         vocab = field.Vocabulary(instance)
-        
+
         def kw_from_value():
-            if self.actb_filter_bogus:   
+            if self.actb_filter_bogus:
                 # delete bogus keywords
                 return vocab.getKey(value.strip(), None)
-            else:                        
+            else:
                 # keep bogus keywords if no keyword, use value
-                return vocab.getKey(value.strip(), value.strip()) 
-            
+                return vocab.getKey(value.strip(), value.strip())
+
         if field.multiValued:
             # make delimiters uniform
             value = value.replace(',', ';')
-            
+
             # get keywords from values
             values = value.split(';')
             result = []
-            
+
             for value in values:
                 keyword = kw_from_value()
                 if keyword and keyword not in result:
                     result.append(keyword)
-                
+
             if field.type!='lines' and field.type!='reference':
-                result= ';'.join(result) 
+                result= ';'.join(result)
         else:
             keyword = kw_from_value()
             result = keyword and keyword or ''
-                
+
         return result, {}
-        
+
 
 from Products.Archetypes.Registry import registerWidget
 
